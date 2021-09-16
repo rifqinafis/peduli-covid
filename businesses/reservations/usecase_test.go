8237d06b995b034b786e3f6f6a145168168895d0
rifqinafis16 <muhammad.nafis@amartha.com>
2021-09-16 08:48:41 +0700

    last progress

--- businesses/reservations/usecase_test.go
+++ businesses/reservations/usecase_test.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"errors"
 	"os"
+	"peduli-covid/businesses/bedtypes"
 	bedtype_mock "peduli-covid/businesses/bedtypes/mocks"
 	hospital_mock "peduli-covid/businesses/hospitals/mocks"
+	"peduli-covid/businesses/invoices"
 	invoice_mock "peduli-covid/businesses/invoices/mocks"
 	notification_mock "peduli-covid/businesses/notifications/mocks"
 	"peduli-covid/businesses/reservations"
@@ -122,6 +124,124 @@ func TestUpdateStatus(t *testing.T) {
 
 		err := reservationUsecase.UpdateStatus(context.Background(), &reservations.Domain{})
 
+		assert.Equal(t, errors.New("error"), err)
+	})
+}
+
+func TestUpdateStatusDone(t *testing.T) {
+	t.Run("test case 1, success test", func(t *testing.T) {
+		reservationDomain := reservations.Domain{
+			ID:         1,
+			UserID:     1,
+			HospitalID: 1,
+			BedtypeID:  1,
+			Status:     "paid",
+		}
+		reservationRepository.On("GetByID", mock.Anything, mock.AnythingOfType("int")).Return(reservationDomain, nil).Once()
+		reservationRepository.On("UpdateStatus", mock.Anything, mock.Anything).Return(nil).Once()
+		bedtypeDomain := bedtypes.Domain{
+			ID:           1,
+			HospitalID:   1101015,
+			Name:         "IGD Khusus Covid",
+			BedAvailable: 0,
+			BedEmpty:     0,
+		}
+		bedtypeRepository.On("GetByID", mock.Anything, mock.AnythingOfType("int")).Return(bedtypeDomain, nil).Once()
+		bedtypeRepository.On("UpdateBedEmpty", mock.Anything, mock.Anything).Return(nil).Once()
+		userDomain := users.Domain{
+			ID:         1,
+			HospitalID: 1,
+			RoleID:     1,
+			Email:      "test@gmail.com",
+			Password:   "test123",
+			Phone:      "081249848950",
+		}
+		userRepository.On("GetByID", mock.Anything, mock.AnythingOfType("int")).Return(userDomain, nil).Once()
+		notificationRepository.On("Store", mock.Anything, mock.Anything).Return(nil).Once()
+
+		err := reservationUsecase.UpdateStatusDone(context.Background(), &reservationDomain)
+
+		assert.Equal(t, nil, err)
+	})
+
+	t.Run("test case 2, repository error test", func(t *testing.T) {
+		reservationRepository.On("GetByID", mock.Anything, mock.AnythingOfType("int")).Return(reservations.Domain{}, errors.New("error")).Once()
+		reservationRepository.On("UpdateStatus", mock.Anything, mock.Anything).Return(errors.New("error")).Once()
+		bedtypeRepository.On("GetByID", mock.Anything, mock.AnythingOfType("int")).Return(bedtypes.Domain{}, errors.New("error")).Once()
+		bedtypeRepository.On("UpdateBedEmpty", mock.Anything, mock.Anything).Return(errors.New("error")).Once()
+		userRepository.On("GetByID", mock.Anything, mock.AnythingOfType("int")).Return(users.Domain{}, errors.New("error")).Once()
+		notificationRepository.On("Store", mock.Anything, mock.Anything).Return(errors.New("error")).Once()
+
+		err := reservationUsecase.UpdateStatusDone(context.Background(), &reservations.Domain{})
+
+		assert.Equal(t, errors.New("error"), err)
+	})
+}
+
+func TestStore(t *testing.T) {
+	t.Run("test case 1, success test", func(t *testing.T) {
+		bedtypeDomain := bedtypes.Domain{
+			ID:           1,
+			HospitalID:   1101015,
+			Name:         "Isolasi Tanpa Tekanan Negatif",
+			BedAvailable: 22,
+			BedEmpty:     14,
+		}
+		bedtypeRepository.On("GetByID", mock.Anything, mock.AnythingOfType("int")).Return(bedtypeDomain, nil).Once()
+		reservationDomain := reservations.Domain{
+			ID:         2,
+			UserID:     3,
+			HospitalID: 1101015,
+			BedtypeID:  4,
+			Status:     "pending",
+		}
+		reservationRepository.On("Store", mock.Anything, mock.Anything).Return(reservationDomain, nil).Once()
+		bedtypeRepository.On("UpdateAvailableBed", mock.Anything, mock.Anything).Return(nil).Once()
+		invoiceDomain := invoices.Domain{
+			ID:            1,
+			ReservationID: 2,
+			Code:          "INVYTGS74TD",
+			Total:         150000,
+			AdminFee:      0,
+		}
+		invoiceRepository.On("Store", mock.Anything, mock.Anything).Return(invoiceDomain, nil).Once()
+		userDomain := users.Domain{
+			ID:         1,
+			HospitalID: 1,
+			RoleID:     1,
+			Email:      "test@gmail.com",
+			Password:   "test123",
+			Phone:      "081249848950",
+		}
+		userRepository.On("GetByID", mock.Anything, mock.AnythingOfType("int")).Return(userDomain, nil).Once()
+		userRepository.On("FindByHospitalID", mock.Anything, mock.AnythingOfType("int")).Return([]users.Domain{userDomain}, nil).Once()
+		notificationRepository.On("Store", mock.Anything, mock.Anything).Return(nil).Once()
+
+		result, err := reservationUsecase.Store(context.Background(), &reservationDomain)
+
+		assert.Equal(t, invoices.Domain{}, result)
+		assert.Equal(t, errors.New("error"), err)
+	})
+
+	t.Run("test case 2, repository error test", func(t *testing.T) {
+		bedtypeRepository.On("GetByID", mock.Anything, mock.AnythingOfType("int")).Return(nil, errors.New("error")).Once()
+		reservationRepository.On("Store", mock.Anything, mock.Anything).Return(nil, errors.New("error")).Once()
+		bedtypeRepository.On("UpdateAvailableBed", mock.Anything, mock.Anything).Return(errors.New("error")).Once()
+		invoiceDomain := invoices.Domain{
+			ID:            1,
+			ReservationID: 2,
+			Code:          "INVYTGS74TD",
+			Total:         150000,
+			AdminFee:      0,
+		}
+		invoiceRepository.On("Store", mock.Anything, mock.Anything).Return(nil, errors.New("error")).Once()
+		userRepository.On("GetByID", mock.Anything, mock.AnythingOfType("int")).Return(nil, errors.New("error")).Once()
+		userRepository.On("FindByHospitalID", mock.Anything, mock.AnythingOfType("int")).Return([]users.Domain{}, errors.New("error")).Once()
+		notificationRepository.On("Store", mock.Anything, mock.Anything).Return(errors.New("error")).Once()
+
+		result, err := reservationUsecase.Store(context.Background(), &reservations.Domain{})
+
+		assert.Equal(t, invoiceDomain, result)
 		assert.Equal(t, errors.New("error"), err)
 	})
 }
--- go.mod
+++ go.mod
@@ -3,15 +3,11 @@ module peduli-covid
 go 1.16
 
 require (
-	github.com/davecgh/go-spew v1.1.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/labstack/echo/v4 v4.5.0
-	github.com/pmezard/go-difflib v1.0.0
 	github.com/spf13/viper v1.8.1
-	github.com/stretchr/objx v0.2.0
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 	gorm.io/driver/postgres v1.1.0
 	gorm.io/gorm v1.21.12
 )
